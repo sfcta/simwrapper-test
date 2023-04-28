a5569f3cb208e83956d03d4d18d1653919abe22e
Chun Ho Chow <15370962+chunhochow@users.noreply.github.com>
2023-04-28 12:26:02 -0700

    change URLs for deployment to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import markdownPlugin from 'vite-plugin-md'
 import pluginRewriteAll from 'vite-plugin-rewrite-all'
 
 export default defineConfig({
-  base: '/',
+  base: '/simwrapper/',
   build: { sourcemap: true },
   plugins: [
     // vue
